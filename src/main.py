d9c57ab5c1e56730be064b4521330f30a4980dd7
Nik <git@mail.vaessen.tech>
2022-09-27 22:08:47 +0200

    try to fix OOM error

--- src/main.py
+++ src/main.py
@@ -334,6 +334,7 @@ def run_train_eval_script(cfg: DictConfig):
         logger=logger,
         callbacks=callbacks,
         profiler=profiler,
+        plugins=SLURMEnvironment(auto_requeue=False),
     )
 
     # construct lighting module for train/test
