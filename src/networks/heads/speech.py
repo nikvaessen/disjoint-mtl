29403f01099a34f9d6c0bffb054da0be16e8346c
Nik <git@mail.vaessen.tech>
2022-10-01 00:35:13 +0200

    set bias correctly

--- src/networks/heads/speech.py
+++ src/networks/heads/speech.py
@@ -4,8 +4,9 @@
 #
 # Author(s): Nik Vaessen
 ########################################################################################
-
-from typing import Union
+import json
+import pathlib
+from typing import Union, Optional
 
 from abc import abstractmethod
 from dataclasses import dataclass
@@ -35,7 +36,12 @@ class SpeechRecognitionHead(t.nn.Module):
 
 @dataclass
 class LinearHeadConfig(CastingConfig):
-    pass
+    blank_token_idx: int
+
+    # optional for setting bias values correctly
+    blank_initial_bias: Optional[int] = None
+    character_distribution_json: Optional[pathlib.Path] = None
+    character_vocabulary_json: Optional[pathlib.Path] = None
 
 
 class LinearHead(SpeechRecognitionHead):
@@ -45,12 +51,49 @@ class LinearHead(SpeechRecognitionHead):
         super().__init__()
 
         self.cfg = cfg
+        self.classification_dim = classification_dim
 
-        self.classification_layer = t.nn.Sequential(
-            t.nn.Linear(
-                in_features=representation_dim,
-                out_features=classification_dim,
-            )
+        self.classification_layer = t.nn.Linear(
+            in_features=representation_dim,
+            out_features=classification_dim,
+        )
+
+        self.classification_layer.bias = self._init_bias(self.classification_layer.bias)
+
+    def _init_bias(self, previous_bias: t.Tensor):
+        if self.cfg.character_vocabulary_json is not None:
+            with self.cfg.character_vocabulary_json.open("r") as f:
+                idx_to_char = json.load(f)["idx_to_char"]
+                idx_to_char = {int(k): v for k, v in idx_to_char.items()}
+        else:
+            idx_to_char = None
+
+        if self.cfg.character_distribution_json is not None:
+            with self.cfg.character_distribution_json.open("r") as f:
+                char_dist = json.load(f)
+        else:
+            char_dist = None
+
+        if char_dist is not None and idx_to_char is not None:
+            assert all(v in char_dist for v in idx_to_char.values())
+            assert len(char_dist) == self.classification_dim
+
+        def get_bias_value(idx):
+            if (
+                idx == self.cfg.blank_token_idx
+                and self.cfg.blank_initial_bias is not None
+            ):
+                return self.cfg.blank_initial_bias
+            elif char_dist is not None and idx_to_char is not None:
+                return char_dist[idx_to_char[idx]]
+            else:
+                return previous_bias[idx]
+
+        bias_values = [get_bias_value(i) for i in range(self.classification_dim)]
+
+        return t.nn.Parameter(
+            t.tensor(bias_values, dtype=previous_bias.dtype),
+            requires_grad=True,
         )
 
     def compute_prediction(self, embedding: t.Tensor) -> t.Tensor:
