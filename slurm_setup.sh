cf406e8b9a39f6fadca33e1d2324abdebf894df6
nvaessen <nvaessen@science.ru.nl>
2023-02-08 13:56:28 +0100

    confusion still

--- slurm_setup.sh
+++ slurm_setup.sh
@@ -1,5 +1,7 @@
 # this file is called before `srun` in the sbatch submission files
 source export_env.sh
+printenv
+
 SLEEP_TIME=${SLURM_ARRAY_TASK_ID:--1}
 SLEEP_TIME=$(($SLEEP_TIME + 1))
 SLEEP_TIME=$(($SLEEP_TIME * 3))
