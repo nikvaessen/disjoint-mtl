276da0e5683e705d16293fa4af35db0461bfbc66
nvaessen <nvaessen@science.ru.nl>
2023-02-08 12:12:14 +0100

    fix issue

---
+++ slurm_setup.sh
@@ -0,0 +1,7 @@
+# this file is called before `srun` in the sbatch submission files
+source export_env.sh
+SLEEP_TIME=${SLURM_ARRAY_TASK_ID:--1}
+SLEEP_TIME=$(($SLEEP_TIME + 1))
+SLEEP_TIME=$(($SLEEP_TIME * 3))
+echo "sleeping for $SLEEP_TIME seconds"
+sleep $SLEEP_TIME
