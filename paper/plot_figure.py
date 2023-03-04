0b8e706fefa9a2738cf9a0c449959d2f1b2f7dd6
nvaessen <nvaessen@science.ru.nl>
2023-03-04 14:27:18 +0100

    eval

--- paper/plot_figure.py
+++ paper/plot_figure.py
@@ -2,11 +2,29 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data.txt', sep='\t')
+df = pd.read_csv("data.txt", sep="\t")
 print(df)
+df["EER"] = df["EER"] * 100
+sns.set_theme()
 
-g = sns.lineplot(df, x='layer', y='EER', hue='weights', marker="o")
-g.set_title("EER on vox2 dev set")
+sns.set(rc={"figure.figsize": (11.7 / 2, 3.2)})
+hue_order = ["pre-trained", "STL", "MTL joint", "MTL dj 2s", "MTL dj 10s"]
+g = sns.lineplot(
+    df,
+    x="layer",
+    y="EER",
+    style="weights",
+    hue="weights",
+    hue_order=hue_order,
+    markers=True,
+    dashes=False
+)
 g.set_xticks([i for i in range(0, 12)])
-g.set_ylim([0, 0.5])
-plt.show()
+g.set_ylim([0, 50])
+g.set(xlabel="Layer from which speaker embedding is computed", ylabel="EER in %")
+g.set_title("EER on development subset of VoxCeleb2")
+sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
+# plt.show()
+
+plt.tight_layout()
+plt.savefig("layer_vs_eer.pdf")
